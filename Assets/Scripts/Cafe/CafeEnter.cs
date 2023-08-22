af33559c4e869e93cb6c49bab409484e45ac0267
R1nge <r1ngegamedev@gmail.com>
2023-08-23 02:41:17 +0300

    Cafe enter

---
+++ Assets/Scripts/Cafe/CafeEnter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Cafe
+{
+    public class CafeEnter : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other) => SceneManager.LoadSceneAsync("GameCafe", LoadSceneMode.Single);
+    }
+}
