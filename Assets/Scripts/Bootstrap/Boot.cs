698f54453d8aa35db80c0640b5d40870acba5167
R1nge <r1ngegamedev@gmail.com>
2023-08-23 02:05:35 +0300

    Game life time

---
+++ Assets/Scripts/Bootstrap/Boot.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Bootstrap
+{
+    public class Boot : MonoBehaviour
+    {
+        private void Start() => SceneManager.LoadSceneAsync("Game", LoadSceneMode.Single);
+    }
+}
---
+++ Assets/Scripts/LifeTimes/GameLifeTime.cs
@@ -0,0 +1,12 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+namespace LifeTimes
+{
+    public class GameLifeTime : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+        }
+    }
+}
